e560a9b3dfcbd7fb9182f9b825cb5fd0ec760696
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-09-14 14:05:44 +0100

    add static, media directory and setting.py and urls.py configured

--- config/settings.py
+++ config/settings.py
@@ -8,24 +8,25 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+Stack: Django 4.2.x, django-allauth, django-crispy-forms + crispy-bootstrap5,
+django-summernote, cloudinary storage, whitenoise, gunicorn,
+dj-database-url + psycopg2.
 """
 
 from pathlib import Path
+from urllib.parse import unquote, urlparse
+
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aj4ieq1-zl%0b67%=8(4#^por2!#q3s^zy#u$24ah9r+tdar+='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost")
 
 
 # Application definition
@@ -37,16 +38,36 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
+
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "django_summernote",
+    "cloudinary_storage",
+    "cloudinary",
+
+    "accounts",
+    "core",
+    "blog",
+    "portfolio",
+    "timesheets",
+    "dashboard",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
+    "accounts.middleware.ForcePasswordChangeMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -54,7 +75,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "core.context_processors.site_settings",
             ],
         },
     },
@@ -74,10 +96,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
@@ -99,25 +121,57 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+SITE_ID = 1
+
+# django-allauth: no public signup — accounts are created by a manager/admin.
+ACCOUNT_ADAPTER = "accounts.adapters.NoSignupAccountAdapter"
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_LOGOUT_ON_GET = True
+
+LOGIN_URL = "account_login"
+LOGIN_REDIRECT_URL = "core:post_login_redirect"
+ACCOUNT_LOGOUT_REDIRECT_URL = "core:home"
+
+
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_URL = 'static/'
+MEDIA_URL = "media/"
+cloudinary_url = config("CLOUDINARY_URL", default="")
+cloudinary_parts = urlparse(cloudinary_url) if cloudinary_url else None
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+if cloudinary_parts and cloudinary_parts.scheme == "cloudinary":
+    CLOUDINARY_STORAGE = {
+        "CLOUD_NAME": cloudinary_parts.hostname or "",
+        "API_KEY": unquote(cloudinary_parts.username or ""),
+        "API_SECRET": unquote(cloudinary_parts.password or ""),
+    }
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+else:
+    CLOUDINARY_STORAGE = {}
+    DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+SUMMERNOTE_THEME = "bs5"
--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("summernote/", include("django_summernote.urls")),
+
+    path("", include("core.urls")),
+    path("blog/", include("blog.urls")),
+    path("portfolio/", include("portfolio.urls")),
+    path("timesheets/", include("timesheets.urls")),
+    path("dashboard/", include("dashboard.urls")),
+    path("account/", include("accounts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.BASE_DIR / "media")
+
