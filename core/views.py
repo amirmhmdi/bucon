826f482761d60e5d0fef51313f64e3c1fc521a08
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-09-20 11:30:07 +0100

    Edit posts in main page to 3

--- core/views.py
+++ core/views.py
@@ -22,7 +22,7 @@ def home(request):
     context = {
         "banners": Banner.objects.filter(is_active=True),
         "featured_projects": Project.objects.filter(status=Project.Status.PUBLISHED, is_featured=True)[:6],
-        "featured_posts": Post.objects.filter(status=Post.Status.PUBLISHED, is_featured=True)[:6],
+        "featured_posts": Post.objects.filter(status=Post.Status.PUBLISHED, is_featured=True)[:3],
         "contact_form": form,
     }
     return render(request, "core/home.html", context)
